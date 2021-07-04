db3e1f9653d902699dba7b9b25d0ada274f728cb
SHU-WEI CHANG <shuwei.chang01@gmail.com>
2021-07-04 20:32:40 +0800

    modify App

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const app = require('./app')
+const app = require('./App')
 
 
 const PORT = process.env.PORT || 3003
